2fd9bffb002afa1b77746ba42949a712f7f5229f
Vasily <pisarevvv@gmail.com>
2021-01-18 18:14:02 +0300

    Basic tests for interface implementation

--- test/interface.jl
+++ test/interface.jl
@@ -1,5 +1,31 @@
-@testset "Interfaces" begin
-    @test SteepestDescent([1, 0]) isa CoreMethod
-    @test HyperGradDescent([1, 0]) isa CoreMethod
-    @test FixedRateDescent([1, 0]) isa CoreMethod
+@testset "Common interface functions"
+    init_vec = [1.0, 0]
+    steepest = SteepestDescent(init_vec)
+    hypergrad = HyperGradDescent(init_vec)
+    fixedrate = FixedRateDescent(init_vec)
+    conjgrad = CGDescent(init_vec)
+    bfgs = BFGS(init_vec)
+    @testset "argumentvec" for descent in (steepest,
+                                           hypergrad,
+                                           fixedrate,
+                                           conjgrad,
+                                           bfgs)
+        @test size(argumentvec(descent)) == size(init_vec)
+    end
+
+    @testset "gradientvec" for descent in (steepest,
+                                           hypergrad,
+                                           fixedrate,
+                                           conjgrad,
+                                           bfgs)
+        @test size(gradientvec(descent)) == size(init_vec)
+    end
+
+    @testset "step_origin" for descent in (steepest,
+                                           hypergrad,
+                                           fixedrate,
+                                           conjgrad,
+                                           bfgs)
+        @test size(step_origin(descent)) == size(init_vec)
+    end
 end
