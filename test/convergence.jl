cd1f85a62026b7198aa0a8ef6fbe4c980e2d6ca7
Степан Захаров <stepanzh@gmail.com>
2022-02-15 17:42:40 +0300

    #29 Tests for custom stop criteria

--- test/convergence.jl
+++ test/convergence.jl
@@ -1,13 +1,48 @@
 @testset "Convergence on the Rosenbrock function" begin
     dim = 2
     x0 = fill(-1, dim)
-    @testset for method in OPT_TYPES
-        opt = method(x0)
-        optresult = optimize!(
-            rosenbrock!, opt, x0;
-            maxiter=1000,
-            constrain_step=(x0,d)->Inf,
-        )
-        @test isapprox(optresult.argument, [1, 1], rtol=0.05)
+    @testset "Default behavior" begin
+        for method in OPT_TYPES
+            opt = method(x0)
+            optresult = optimize!(
+                rosenbrock!, opt, x0;
+                maxiter=1000,
+                constrain_step=(x0,d)->Inf,
+            )
+            @testset "$method" begin
+                @test optresult.converged
+                @test optresult.argument ≈ [1, 1] rtol=0.05
+            end
+        end
+    end
+    @testset "By x - xpre" begin
+        for method in OPT_TYPES
+            opt = method(x0)
+            optresult = optimize!(
+                rosenbrock!, opt, x0;
+                maxiter=1000,
+                constrain_step=(x0,d)->Inf,
+                convcond=(x, xpre, y, ypre, g) -> norm(x - xpre, 2) ≤ 1000*eps(),
+            )
+            @testset "$method" begin
+                @test optresult.converged
+                @test optresult.argument ≈ [1, 1] rtol=0.05
+            end
+        end
+    end
+    @testset "By y - ypre" begin
+        for method in OPT_TYPES
+            opt = method(x0)
+            optresult = optimize!(
+                rosenbrock!, opt, x0;
+                maxiter=1000,
+                constrain_step=(x0,d)->Inf,
+                convcond=(x, xpre, y, ypre, g) -> abs(y - ypre) ≤ 1000*eps(),
+            )
+            @testset "$method" begin
+                @test optresult.converged
+                @test optresult.argument ≈ [1, 1] rtol=0.05
+            end
+        end
     end
 end
